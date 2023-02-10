46a3dc1203ff114315d8e319e1392d325f6ca487
Ciprian <ciprian.iliuta05@gmail.com>
2023-02-10 21:21:48 +0200

    final

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function openNav() {
     
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidenav").style.width = "200px";
+    document.getElementById("main").style.marginLeft = "200px";
   }
   
   /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
@@ -9,3 +9,14 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
   } 
+  function myFunction() {
+    var content = document.getElementById('textArea').innerHTML;
+navigator.clipboard.writeText(content)
+.then(() => {
+console.log("Text has been copied!")
+})
+.catch(err => {
+console.log('Something is not right', 'Oops!');
+})
+      
+	  }
